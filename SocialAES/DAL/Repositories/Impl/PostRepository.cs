475a56c2437853cfd45cdbc24611fc1e2f8822c7
irtm6 <175nosart@gmail.com>
2024-12-10 20:58:30 +0200

    Add Post Repository

---
+++ SocialAES/DAL/Repositories/Impl/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories.Impl
+{
+    public class PostRepository
+        : BaseRepository<Post>, IPostRepository
+    {
+        internal PostRepository(SocialAESContext context)
+                : base(context)
+        {
+
+        }
+    }
+}
