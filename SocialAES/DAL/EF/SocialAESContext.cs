47a9541ce40564b0090bc1fe75efcb667eb1d80e
irtm6 <175nosart@gmail.com>
2024-12-10 20:57:37 +0200

    Added SocialAESContext

---
+++ SocialAES/DAL/EF/SocialAESContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.EF
+{
+    public class SocialAESContext 
+        : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public SocialAESContext(DbContextOptions options) 
+            : base(options) 
+        {
+        }
+
+    }
+}
