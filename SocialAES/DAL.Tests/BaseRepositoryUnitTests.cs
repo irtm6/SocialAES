b0333258a77eb8771d859c9081c5daa825f39bdd
irtm6 <175nosart@gmail.com>
2024-12-17 16:12:02 +0200

    Add tests for post repositories

---
+++ SocialAES/DAL.Tests/BaseRepositoryUnitTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using DAL.EF;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace DAL.Tests;
+
+    public class BaseRepositoryUnitTests
+    {
+    [Fact]
+    public void Create_InputPostInstance_CalledAddMethodOfDBSetWithPostInstance() 
+    {
+        //Arrange
+        DbContextOptions options = new DbContextOptionsBuilder<SocialAESContext>()
+            .Options;
+        var mockContext = new Mock<SocialAESContext>(options);
+        var mockDbSet = new Mock<DbSet<Post>>();
+        mockContext
+           .Setup(context =>
+           context.Set<Post>(
+                 ))
+           .Returns(mockDbSet.Object);
+        var repository = new TestPostRepository(mockContext.Object);
+        Post expectedPost = new Mock<Post>().Object;
+
+        //Act
+        repository.Create(expectedPost);
+        //Assert
+        mockDbSet.Verify(
+            dbSet => dbSet.Add(
+                expectedPost
+                ), Times.Once());
+    }
+    [Fact]
+    public void Get_InputId_CalledFindMethodOfDBSetWithCorrectId()
+    {
+        // Arrange
+        DbContextOptions options = new DbContextOptionsBuilder<SocialAESContext>()
+        .Options;
+        var mockContext = new Mock<SocialAESContext>(options);
+        var mockDbSet = new Mock<DbSet<Post>>();
+        mockContext
+        .Setup(context =>
+        context.Set<Post>(
+        ))
+        .Returns(mockDbSet.Object);
+        Post expectedPost = new Post() { postID = 1 };
+        mockDbSet.Setup(mock => mock.Find(expectedPost.postID))
+        .Returns(expectedPost);
+        var repository = new TestPostRepository(mockContext.Object);
+        //Act
+        var actualPost = repository.Get(expectedPost.postID);
+        // Assert
+        mockDbSet.Verify(
+        dbSet => dbSet.Find(
+        expectedPost.postID
+        ), Times.Once());
+        Assert.Equal(expectedPost, actualPost);
+    }
+    [Fact]
+    public void Delete_InputId_CalledFindAndRemoveMethodsOfDBSetWithCorrectArg()
+    {
+        // Arrange
+        DbContextOptions options = new DbContextOptionsBuilder<SocialAESContext>()
+        .Options;
+        var mockContext = new Mock<SocialAESContext>(options);
+        var mockDbSet = new Mock<DbSet<Post>>();
+        mockContext
+        .Setup(context =>
+        context.Set<Post>(
+        ))
+        .Returns(mockDbSet.Object);
+        var repository = new TestPostRepository(mockContext.Object);
+        Post expectedPost = new Post() { postID = 1 };
+        mockDbSet.Setup(mock => mock.Find(expectedPost.postID))
+        .Returns(expectedPost);
+        //Act
+        repository.Delete(expectedPost.postID);
+        // Assert
+        mockDbSet.Verify(
+        dbSet => dbSet.Find(
+        expectedPost.postID
+        ), Times.Once());
+        mockDbSet.Verify(
+        dbSet => dbSet.Remove(
+        expectedPost
+        ), Times.Once());
+    }
+}
+
